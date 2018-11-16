47e13404c05e5868478d4bd8b7ecf191ff93fb89
ParkChunSo <cnsth123@gmail.com>
2018-11-16 15:54:28 +0900

    Initial commit

---
+++ src/main/java/kr/ac/skuniv/realestate/RealestateApplication.java
@@ -0,0 +1,12 @@
+package kr.ac.skuniv.realestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealestateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealestateApplication.class, args);
+    }
+}
