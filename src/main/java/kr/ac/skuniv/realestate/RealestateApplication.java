f0f3b056ac7562fb709333fe680ad7f4c413e13e
kimYSang <qwdbstkd@naver.com>
2018-11-16 18:14:23 +0900

    test

--- src/main/java/kr/ac/skuniv/realestate/RealestateApplication.java
+++ src/main/java/kr/ac/skuniv/realestate/RealestateApplication.java
@@ -9,5 +9,6 @@ public class RealestateApplication {
     public static void main(String[] args) {
         SpringApplication.run(RealestateApplication.class, args);
         System.out.println("chunso");
+        
     }
 }
