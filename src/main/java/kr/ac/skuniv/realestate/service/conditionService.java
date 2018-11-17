c5b09b539a691ac5480e98d3e32f44ad723263bc
ParkChunSo <cnsth123@gmail.com>
2018-11-17 14:20:24 +0900

    11

---
+++ src/main/java/kr/ac/skuniv/realestate/service/conditionService.java
@@ -0,0 +1,11 @@
+package kr.ac.skuniv.realestate.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class conditionService {
+
+
+}
