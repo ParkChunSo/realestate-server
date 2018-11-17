162bf433bc4bde6c7cc4bce1dd6aac0d3c368908
kimYSang <qwdbstkd@naver.com>
2018-11-17 22:03:14 +0900

    DB connect & query test

--- src/main/java/kr/ac/skuniv/realestate/RealestateApplication.java
+++ src/main/java/kr/ac/skuniv/realestate/RealestateApplication.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class RealestateApplication {
+
+    @PostConstruct
+    public void initApplication(){
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RealestateApplication.class, args);
--- src/main/java/kr/ac/skuniv/realestate/controller/conditionController.java
+++ src/main/java/kr/ac/skuniv/realestate/controller/conditionController.java
@@ -1,5 +1,8 @@
 package kr.ac.skuniv.realestate.controller;
 
+import kr.ac.skuniv.realestate.repository.ConditionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "realestate/condition/*")
 public class conditionController {
 
+    @Autowired
+    private ConditionRepository conditionRepository;
+
     @GetMapping("/{region}")
     public String onlyRegion(@PathVariable String region){
         System.out.println(region);
-        return region;
+        return conditionRepository.getPriceSum() + "";
     }
 
     @GetMapping("/{region}/{term}")
