d8b1cf6817d257ae2b84e8fd0d2ad3c1fc550322
kimYSang <qwdbstkd@naver.com>
2018-11-17 22:55:50 +0900

    swagger 적용

--- src/main/java/kr/ac/skuniv/realestate/controller/conditionController.java
+++ src/main/java/kr/ac/skuniv/realestate/controller/conditionController.java
@@ -1,12 +1,14 @@
 package kr.ac.skuniv.realestate.controller;
 
+import kr.ac.skuniv.realestate.domain.WebForsaleTbl;
 import kr.ac.skuniv.realestate.repository.ConditionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "realestate/condition/*")
@@ -15,10 +17,10 @@ public class conditionController {
     @Autowired
     private ConditionRepository conditionRepository;
 
-    @GetMapping("/{region}")
-    public String onlyRegion(@PathVariable String region){
-        System.out.println(region);
-        return conditionRepository.getPriceSum() + "";
+    @GetMapping("/{code}")
+    public List<WebForsaleTbl> onlyRegion(@PathVariable int code){
+        System.out.println(code);
+        return conditionRepository.findAllByCode(code);
     }
 
     @GetMapping("/{region}/{term}")
--- src/main/java/kr/ac/skuniv/realestate/repository/ConditionRepository.java
+++ src/main/java/kr/ac/skuniv/realestate/repository/ConditionRepository.java
@@ -5,12 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ConditionRepository extends JpaRepository<WebForsaleTbl, Long> {
 
     @Query("select avg(f.price) from WebForsaleTbl f where f.code=1122333")
     int getPriceSum();
 
-
-
+    List<WebForsaleTbl> findAllByCode(int code);
 }
