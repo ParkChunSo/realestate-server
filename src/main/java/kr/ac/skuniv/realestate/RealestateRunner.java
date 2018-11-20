d442a11d15dc3087318203f921bd9ee5928b51c3
kimYSang <qwdbstkd@naver.com>
2018-11-20 21:42:56 +0900

    read excel 수정

--- src/main/java/kr/ac/skuniv/realestate/RealestateRunner.java
+++ src/main/java/kr/ac/skuniv/realestate/RealestateRunner.java
@@ -1,6 +1,5 @@
 package kr.ac.skuniv.realestate;
 
-import kr.ac.skuniv.realestate.util.ReadXLSX;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
--- src/main/java/kr/ac/skuniv/realestate/controller/ConditionController.java
+++ src/main/java/kr/ac/skuniv/realestate/controller/ConditionController.java
@@ -2,17 +2,13 @@ package kr.ac.skuniv.realestate.controller;
 
 import kr.ac.skuniv.realestate.domain.entity.Forsale;
 import kr.ac.skuniv.realestate.repository.ForsaleRepository;
-import kr.ac.skuniv.realestate.util.CodeModel;
-import kr.ac.skuniv.realestate.util.ReadXLSX;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping(value = "realestate/condition/*")
@@ -20,9 +16,6 @@ public class ConditionController {
 
     @Autowired
     private ForsaleRepository forsaleRepository;
-
-    @Autowired
-    ReadXLSX readXLSX;
 
     @GetMapping("/{region}")
     public List<Forsale> onlyRegion(@PathVariable String region){
@@ -34,15 +27,5 @@ public class ConditionController {
     public String termRegion(@PathVariable String region, @PathVariable String term){
         System.out.println(region+term);
         return region + term;
-    }
-
-    @GetMapping("/code")
-    public List<CodeModel> code(){
-        try {
-            return readXLSX.separateCode(readXLSX.readXLSX());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
