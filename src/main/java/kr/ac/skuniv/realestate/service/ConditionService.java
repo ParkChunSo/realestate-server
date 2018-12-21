25c2629cb9f8c9e0701f49e45e9ee148c1d0166b
kimYSang <qwdbstkd@naver.com>
2018-12-22 07:21:01 +0900

    redis 연동

--- src/main/java/kr/ac/skuniv/realestate/service/ConditionService.java
+++ src/main/java/kr/ac/skuniv/realestate/service/ConditionService.java
@@ -190,5 +190,4 @@ public class ConditionService {
         }
         return mapDtos;
     }
-
 }
--- src/test/java/kr/ac/skuniv/realestate/RealestateApplicationTests.java
+++ src/test/java/kr/ac/skuniv/realestate/RealestateApplicationTests.java
@@ -1,12 +1,15 @@
 package kr.ac.skuniv.realestate;
 
+import kr.ac.skuniv.realestate.domain.entity.RegionCode;
 import kr.ac.skuniv.realestate.repository.ForsaleRepository;
+import kr.ac.skuniv.realestate.repository.RegionCodeRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
@@ -19,13 +22,16 @@ public class RealestateApplicationTests {
     DataSource dataSource;
 
     @Autowired
-    ForsaleRepository forsaleRepository;
+    RegionCodeRepository regionCodeRepository;
+
+    @MockBean
+    RegionCode regionCode;
 
     private Logger logger = LoggerFactory.getLogger(RealestateApplicationTests.class);
     @Test
     public void contextLoads() {
-
-        //logger.info(forsaleRepository.findByCode(1100000));
+        //regionCode = regionCodeRepository.findByCode(11);
+        //logger.info(regionCode.getRegion());
 
     }
 
