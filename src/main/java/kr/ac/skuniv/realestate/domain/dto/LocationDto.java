e3350e7605a122d4ccd5e2feca22b7dc59e9ae4b
kimYSang <qwdbstkd@naver.com>
2019-03-03 13:06:09 +0900

    190303 배포

--- src/main/java/kr/ac/skuniv/realestate/domain/dto/LocationDto.java
+++ src/main/java/kr/ac/skuniv/realestate/domain/dto/LocationDto.java
@@ -2,11 +2,12 @@ package kr.ac.skuniv.realestate.domain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
-@Getter @Setter @AllArgsConstructor
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
 public class LocationDto {
     private BigDecimal latitude;
     private BigDecimal longitude;
--- src/main/java/kr/ac/skuniv/realestate/domain/dto/MapLocationDto.java
+++ src/main/java/kr/ac/skuniv/realestate/domain/dto/MapLocationDto.java
@@ -2,9 +2,10 @@ package kr.ac.skuniv.realestate.domain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter @Setter @AllArgsConstructor
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
 public class MapLocationDto {
     private LocationDto rightTop;
 
